23325056b0939304e204ef673b3682acc5f43b95
sigmapng <riabkoyuliia@gmail.com>
2023-10-01 19:18:36 +0300

    1st lab

---
+++ main.py
@@ -0,0 +1,18 @@
+# Dictionary to store user inputs
+userData = {
+    "What is your name?": "Your name is",
+    "How old are you?": "You are",
+    "Where do you live": "You live in",
+    "Where do you study?": "You study at",
+    "What is the number of your group?": "Your group number is",
+    "What is your number in the list of the students": "Your list number is",
+    "What was your ZNO results in Ukrainian?": "Your result was",
+    "Are you planning on getting master degree? ": "Masters degree",
+}
+
+# Collect user inputs
+userInputs = {question: input(question) for question in userData.keys()}
+
+# Print user data
+for question, label in userData.items():
+    print(f"{label}: {userInputs[question]}")
